639fcfb2e9179a93f8a897a570ac8a6a3be96e6f
OttoMaticCoder <123340921+OttoMaticCoder@users.noreply.github.com>
2024-06-22 11:24:23 -0700

    Assignment 1
    Everything you want!

---
+++ src/com/coderscamp/assignment2/Main.java
@@ -0,0 +1,51 @@
+package com.coderscamp.assignment2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+	
+public static void main(String[] agrs) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Pick a number between 1-100");
+		String userInput = scanner.nextLine();
+		Integer convertedInput = Integer.parseInt(userInput);
+		Integer winCondition = (int) (Math.random() * 10 + 1);
+		
+		int i = 0;
+		while(i < 4) {
+			if(convertedInput == winCondition) {
+				break;	
+			}
+			
+			else if (convertedInput < 1 || convertedInput > 100 ) {
+				System.out.println("Invalid Input, No Chance used. Try Again: ");
+				userInput = scanner.nextLine();
+				convertedInput = Integer.parseInt(userInput);
+			}
+			
+			else if(convertedInput > winCondition) {
+				i++;
+				System.out.println(convertedInput + " is Wrong guess Lower!");
+				userInput = scanner.nextLine();
+				convertedInput = Integer.parseInt(userInput);
+			}
+				
+			else if (convertedInput < winCondition) {
+				i++;
+
+				System.out.println(convertedInput + " is Wrong guess Lower!");
+				userInput = scanner.nextLine();
+				convertedInput = Integer.parseInt(userInput);
+				}
+		}
+		
+		if(convertedInput == winCondition) {
+			System.out.println("You Win!");
+			
+		} else {
+			System.out.println("You Lose! Winning Number was: " + winCondition);
+		}
+	}
+}
